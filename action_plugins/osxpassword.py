f045e0cc7da4d2a5795eb283e74d8029a5810301
Benjamin R. Haskell <ansible@benizi.com>
2017-06-14 15:23:38 -0400

    Add a bunch of osxpassword tests
    Added various items when trying to figure out why it wasn't working. Run
    them if the module is run on its own.

--- action_plugins/osxpassword.py
+++ action_plugins/osxpassword.py
@@ -61,6 +61,47 @@ class ActionModule(ActionBase):
 
 
 if __name__ == '__main__':
-    hashed = do_encrypt('asdf', 'pbkdf2_sha512_osx')
-    verify = pbkdf2_sha512_osx.verify('asdf', hashed)
-    print('{"hashed":"%s","ok":"%s"}' % (hashed, verify))
+    from base64 import b64decode
+    from os import environ
+    from json import dumps
+
+    # TODO: is this in stdlib somewhere?
+    def slice(dict, *keys):
+        return {key: dict[key] for key in keys if key in dict}
+
+    def results(hash, ok):
+        r = slice(locals(), 'hash', 'ok')
+        part_names = ['blank', 'algo', 'rounds', 'salt', 'entropy']
+        r.update(dict(zip(part_names, hash.split('$'))))
+        return r
+
+    digest = pbkdf2_sha512_osx
+    static_password = 'asdf'
+    predefined_salt = b64decode('RKi19p7zfs/5PyeEsNaaEwJAaE2pFaJ0rpWydk7J2bs=')
+    env_pass = environ.get('password')
+    env_hash = environ.get('hash')
+
+    tests = dict(ok={})
+
+    def do_test(name, password, hashed=None, salt=None):
+        if not hashed:
+            kwargs = {'salt':salt} if salt else {}
+            hashed = do_encrypt(password, digest.name, **kwargs)
+        ok = digest.verify(password, hashed)
+        tests[name] = results(hashed, ok)
+        tests['ok'][name] = ok
+
+    # Use a static password
+    do_test('static_pre', static_password, salt=predefined_salt)
+    do_test('static_rand', static_password)
+
+    # Try the password passed in env vars
+    if env_pass:
+        do_test('env_pre', env_pass, salt=predefined_salt)
+        do_test('env_rand', env_pass)
+
+    # Try the password and hash passed in env vars
+    if env_pass and env_hash:
+        do_test('env_env', env_pass, hashed=env_hash)
+
+    print(dumps(tests))
